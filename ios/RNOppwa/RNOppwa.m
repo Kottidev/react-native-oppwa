286c60682eed44cafacf752a9f5cb1e028393899
Mohamed Anouar KOTTI <kottianouar@gmail.com>
2019-01-27 11:24:20 +0100

    Add status transaction

--- ios/RNOppwa/RNOppwa.m
+++ ios/RNOppwa/RNOppwa.m
@@ -11,11 +11,11 @@ RCT_EXPORT_MODULE(RNOppwa);
 {
     self = [super init];
     if (self) {
-      #ifdef DEBUG
-        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeTest];
-     #else
-        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeTest];
-     #endif
+#ifdef DEBUG
+        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeLive];
+#else
+        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeLive];
+#endif
     }
     
     return self;
@@ -27,38 +27,40 @@ RCT_EXPORT_MODULE(RNOppwa);
 RCT_EXPORT_METHOD(transactionPayment: (NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     
     NSError * _Nullable error;
-   
+    
     NSMutableDictionary *result = [[NSMutableDictionary alloc]initWithCapacity:10];
     OPPCardPaymentParams *params = [OPPCardPaymentParams cardPaymentParamsWithCheckoutID:[options valueForKey:@"checkoutID"]
-
-                                                                        paymentBrand:[options valueForKey:@"paymentBrand"]
-                                                                              holder:[options valueForKey:@"holderName"]
-                                                                              number:[options valueForKey:@"cardNumber"]
-                                                                         expiryMonth:[options valueForKey:@"expiryMonth"]
-                                                                          expiryYear:[options valueForKey:@"expiryYear"]
-                                                                                 CVV:[options valueForKey:@"cvv"]
-                                                                               error:&error];
-
+                                    
+                                                                            paymentBrand:[options valueForKey:@"paymentBrand"]
+                                                                                  holder:[options valueForKey:@"holderName"]
+                                                                                  number:[options valueForKey:@"cardNumber"]
+                                                                             expiryMonth:[options valueForKey:@"expiryMonth"]
+                                                                              expiryYear:[options valueForKey:@"expiryYear"]
+                                                                                     CVV:[options valueForKey:@"cvv"]
+                                                                                   error:&error];
+    
     if (error) {
-      reject(@"oppwa/card-init",error.localizedDescription, error);
+        reject(@"oppwa/card-init",error.localizedDescription, error);
     } else {
-      params.tokenizationEnabled = YES;
-      OPPTransaction *transaction = [OPPTransaction transactionWithPaymentParams:params];
-
-      [provider submitTransaction:transaction completionHandler:^(OPPTransaction * _Nonnull transaction, NSError * _Nullable error) {
-        if (transaction.type == OPPTransactionTypeAsynchronous) {
-          // Open transaction.redirectURL in Safari browser to complete the transaction
-            [result setObject:[NSString stringWithFormat:@"asynchronous"] forKey:@"type"];
-            [result setObject:[NSString stringWithFormat:@"%@",transaction.redirectURL] forKey:@"redirectURL"];
-            resolve(result);
-        }  else if (transaction.type == OPPTransactionTypeSynchronous) {
-            [result setObject:[NSString stringWithFormat:@"synchronous"] forKey:@"type"];
-            resolve(result);
-        } else {
-          reject(@"oppwa/transaction",error.localizedDescription, error);  
-          // Handle the error
-        }
-      }];
+        params.tokenizationEnabled = YES;
+        OPPTransaction *transaction = [OPPTransaction transactionWithPaymentParams:params];
+        
+        [provider submitTransaction:transaction completionHandler:^(OPPTransaction * _Nonnull transaction, NSError * _Nullable error) {
+            if (transaction.type == OPPTransactionTypeAsynchronous) {
+                // Open transaction.redirectURL in Safari browser to complete the transaction
+                [result setObject:[NSString stringWithFormat:@"transactionCompleted"] forKey:@"status"];
+                [result setObject:[NSString stringWithFormat:@"asynchronous"] forKey:@"type"];
+                [result setObject:[NSString stringWithFormat:@"%@",transaction.redirectURL] forKey:@"redirectURL"];
+                resolve(result);
+            }  else if (transaction.type == OPPTransactionTypeSynchronous) {
+                [result setObject:[NSString stringWithFormat:@"transactionCompleted"] forKey:@"status"];
+                [result setObject:[NSString stringWithFormat:@"synchronous"] forKey:@"type"];
+                resolve(result);
+            } else {
+                reject(@"oppwa/transaction",error.localizedDescription, error);
+                // Handle the error
+            }
+        }];
     }
 }
 /**
@@ -66,20 +68,21 @@ RCT_EXPORT_METHOD(transactionPayment: (NSDictionary*)options resolver:(RCTPromis
  * @return
  */
 RCT_EXPORT_METHOD(isValidNumber:
-            (NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-
-        
-        if ([OPPCardPaymentParams isNumberValid:[options valueForKey:@"cardNumber"] forPaymentBrand:[options valueForKey:@"paymentBrand"]]) {
-            resolve([NSNull null]);
-        }
-        else {
-           reject(@"oppwa/card-invalid", @"The card number is invalid.", nil);
-        }
+                  (NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+    
+    
+    if ([OPPCardPaymentParams isNumberValid:[options valueForKey:@"cardNumber"] forPaymentBrand:[options valueForKey:@"paymentBrand"]]) {
+        resolve([NSNull null]);
+    }
+    else {
+        reject(@"oppwa/card-invalid", @"The card number is invalid.", nil);
+    }
 }
 
 + (BOOL)requiresMainQueueSetup
 {
     return YES;
 }
+
 
 @end
